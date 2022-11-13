fb49b7b97511e8c6a75ce420819cdb8d7b919a0b
Wojciech Siudy <wojciechsiudy@hotmail.com>
2022-11-13 09:05:42 +0100

    Reorganize code

--- main.go
+++ main.go
@@ -2,8 +2,38 @@ package main
 
 import(
 	"github.com/wojciechsi/grzejemy.pl/controller"
+	"github.com/wojciechsi/grzejemy.pl/models"
 )
 
 func main () {
-	controller.Run()
+	//controller.Run()
+	jacuśData := UserData{ 					//delare nested
+		name:     "Jacek Szedel", 			//struct
+		password: "1234",
+	}
+
+	jacuś := User{ 							//declare nesting
+		UserData:      jacuśData, 			//class
+		favouriteFuel: Opony,
+	}
+
+	var sklepyUJacka []string
+
+	jacuśHandlarz := Vendor{
+		UserData:    jacuśData,
+		salesPoints: sklepyUJacka,
+	}
+
+	var users []Userer 						//polymorphic vector
+
+	users = append(users, jacuś)
+	users = append(users, jacuśHandlarz)
+
+	fmt.Println(jacuś.getName()) 			//call method from
+											//nested struct
+											//as it would be
+											//inherited
+
+	fmt.Println(users[0].getName()) 		//getName must be declared
+											//in interface definiction
 }
---
+++ models/go.mod
@@ -0,0 +1,3 @@
+module github.com/wojciechsi/grzejemy.pl/models
+
+go 1.19
