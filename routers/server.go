be271c845081366df90d72c60668bfb38debb690
JakubCiolek <44441408+JakubCiolek@users.noreply.github.com>
2022-11-19 19:32:21 +0100

    frontend part 2

--- routers/server.go
+++ routers/server.go
@@ -4,28 +4,26 @@ import (
 	"fmt"
 	"net/http"
 	"html/template"
-	"github.com/wojciechsi/grzejemy.pl/models"
+	//"github.com/wojciechsi/grzejemy.pl/models"
 )
 
+type cat struct {
+	Name string
+	Question string
+}
 func homePageHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Dorzuć do pieca!")
 }
 
 func offerHandler(w http.ResponseWriter, r *http.Request) {
-	
-//	jacuś := models.NewBuyer("Jacek")
-	jacek := models.NewVendor("dr Szedel")
-	jacex := models.NewSalesPoint("Gliwice", jacek)
-
-	taniosprzedam := models.NewOffer(jacex, models.FuelType{Name: "groszek"}, 3560.75)
-
+	p := cat{Name: "Mr.Meow", Question: "I can has cheezburger?"}
 	t, _ := template.ParseFiles("offers.html")
-	t.Execute(w, taniosprzedam)
+	t.Execute(w, p)
 }
 
 func RunServer() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.HandlerFunc(homePageHandler))
+	mux.Handle("/offer/", http.HandlerFunc(offerHandler))
 	http.ListenAndServe(":8080", mux)
-	mux.Handle("/offer", http.HandlerFunc(offerHandler))
 }
